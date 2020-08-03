d6ca9b62d0bc7a5e11463046ab1a0dd3543c67a3
wingdust <995315823@qq.com>
2020-08-03 16:00:34 +0800

    test

--- private/WingDust/keybindings.el
+++
@@ -1,6 +0,0 @@
-
-;; Do what i mean
-(global-set-key (kbd "M-s o") 'occur-dwim)
-
-;; 有道字典按键绑定
-;;(spacemacs/set-leader-keys  "oy" 'youdao-dictionary-search-at-point+)
--- site-lisp/lyqi/MidiScript/AppDelegate.m
+++
@@ -1,9 +0,0 @@
-#import "AppDelegate.h"
-#import "MidiController.h"
-
-@implementation AppDelegate
-- (void) playNote: (int) pitch
-{
-  [midiController playPitch:pitch withLength:200 withVelocity:80];
-}
-@end
